ec5ff0648bb34d7c0382a81ccadf22d2c79b974b
Nguyễn Quang Huy <huynq@avada.email>
2023-06-08 01:49:29 +0700

    fix deploy react router not working

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import AddEditPage from 'features/Photo/pages/AddEdit';
 import Main from 'features/Photo/pages/Main';
 import Home from 'pages/Home';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
-      <Routes>
+      <HashRouter>
         <Route exact path="/" element={<Home />} />
         <Route path="/photos" element={<Photo />}>
           <Route path={'/photos/'} element={<Main />} />
@@ -19,7 +19,7 @@ function App() {
         </Route>
         {/* <Route exact path="/sign-in" element={<SignIn />} /> */}
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </HashRouter>
     </React.Fragment>
   );
 }
